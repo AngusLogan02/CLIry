8d6494e55546ac9da10d2b1aa42c83e47a15b853
Angus Logan <angus.logan@baronsview.com>
2021-12-24 22:09:41 +0000

    added user authentication

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from main.models import User
 
 class SignupForm(FlaskForm):
     username = StringField("USERNAME:", validators=[DataRequired(), Length(min=3, max=24)])
@@ -8,6 +9,15 @@ class SignupForm(FlaskForm):
     password = PasswordField("PASSWORD:", validators=[DataRequired(), Length(min=8)])
     confirm_password = PasswordField('CONFIRM PASSWORD:', validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField('SIGN UP')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("An account already exists with that username.")
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError("An account already exists with that email.")
 
 class LoginForm(FlaskForm):
     username = StringField("USERNAME:", validators=[DataRequired(), Length(min=3, max=24)])
--- main/routes.py
+++ main/routes.py
@@ -1,28 +1,45 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from main.forms import SignupForm, LoginForm
 from main import app, db, bcrypt
 from main.models import User, Post
-
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.username.data == 'angus' and form.password.data == 'password':
-            return redirect(url_for("index"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("index"))
         else:
             flash("Login unsuccessful. Please check username and password.", "fail")
     return render_template("login.html", form=form)
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = SignupForm()
     if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
         flash(f"Account {form.username.data} created. Log in.", "success")
         return redirect(url_for("login"))
 
     return render_template("signup.html", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
