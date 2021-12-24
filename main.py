27f130eed8d1eabdda2d6ea3967fb2728a01f490
Angus Logan <angus.logan@baronsview.com>
2021-12-24 17:54:14 +0000

    linked login and signup pages together

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
